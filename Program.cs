90b74cbe3ad02a50fa530ff36536d860eaae9d53
Hedius <git@hedius.eu>
2023-08-11 23:40:21 +0200

    add vip challenge example

--- Program.cs
+++ Program.cs
@@ -1,83 +1,109 @@
 ﻿using BattleBitAPI;
 using BattleBitAPI.Common;
 using BattleBitAPI.Server;
-using System.Threading.Channels;
-using System.Xml;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        var listener = new ServerListener<MyPlayer, MyGameServer>();
+        var listener = new ServerListener<MyPlayer, VipHammerChallengeServer>();
         listener.Start(29294);
 
         Thread.Sleep(-1);
     }
 }
-class MyPlayer : Player<MyPlayer>
+
+internal class MyPlayer : Player<MyPlayer>
 {
-    public bool IsZombie;
+    // ToDo not used..
+    public bool IsVIP;
 }
-class MyGameServer : GameServer<MyPlayer>
+
+internal class VipHammerChallengeServer : GameServer<MyPlayer>
 {
+    private const int NeededPlayers = 6;
+    private MyPlayer _vip;
+
 
     public override async Task OnRoundStarted()
     {
+        StartChallenge();
     }
+
     public override async Task OnRoundEnded()
     {
+        if (_vip != null) AnnounceLong($"{_vip.Name} and their team have one the challenge!");
+        _vip = null;
     }
 
     public override async Task OnPlayerConnected(MyPlayer player)
     {
-        bool anyZombiePlayer = false;
-        foreach (var item in AllPlayers)
+        if (CurrentPlayers < NeededPlayers)
+            SayToChat($"{player.Name} has joined the server. {CurrentPlayers}/{NeededPlayers} of required players.");
+    }
+
+    public override async Task OnPlayerDisconnected(MyPlayer player)
+    {
+        if (RoundSettings.State == GameState.Playing && _vip == player)
         {
-            if (item.IsZombie)
+            AnnounceLong($"VIP {player.Name} has left. Choosing a new one.");
+            StartChallenge();
+        }
+    }
+
+    private void StartChallenge()
+    {
+        var random = new Random();
+        var players = AllPlayers.ToList();
+        // We already have a vip... try to redraw in the same team.
+        if (_vip != null)
+        {
+            var tries = 0;
+            while (true)
             {
-                anyZombiePlayer = true;
-                break;
+                var j = random.Next(players.Count);
+                if (players[j].Team != _vip.Team)
+                {
+                    tries++;
+                    continue;
+                }
+
+                if (tries >= 10)
+                {
+                    AnnounceShort("Restarting round. Unable to determine a new VIP");
+                    RoundSettings.SecondsLeft = 5;
+                    return;
+                }
+
+                _vip = players[j];
             }
         }
 
-        if (!anyZombiePlayer)
-        {
-            player.IsZombie = true;
-            player.Message("You are the zombie.");
-            player.Kill();
-        }
+        _vip = players[random.Next(players.Count)];
+        // First vip of the round. kill everyone to reconfigure their loadouts.
+        foreach (var player in AllPlayers) player.Kill();
+
+        AnnounceLong($"{_vip.Name} is the VIP!");
     }
 
     public override async Task OnAPlayerKilledAnotherPlayer(OnPlayerKillArguments<MyPlayer> args)
     {
-        if (args.Victim.IsZombie)
+        if (args.Victim == _vip)
         {
-            args.Victim.IsZombie = false;
-            args.Victim.Message("You are no longer zombie");
-
-            AnnounceShort("Choosing new zombie in 5");
-            await Task.Delay(1000);
-            AnnounceShort("Choosing new zombie in 4");
-            await Task.Delay(1000);
-            AnnounceShort("Choosing new zombie in 3");
-            await Task.Delay(1000);
-            AnnounceShort("Choosing new zombie in 2");
-            await Task.Delay(1000);
-            AnnounceShort("Choosing new zombie in 1");
-            await Task.Delay(1000);
-
-            args.Killer.IsZombie = true;
-            args.Killer.SetHeavyGadget(Gadgets.SledgeHammer.ToString(), 0, true);
-
-            var position = args.Killer.GetPosition();
+            AnnounceLong($"{args.Killer.Name} has killed the VIP ${_vip.Name} and wins the challenge!");
+            RoundSettings.SecondsLeft = 10;
         }
     }
 
-
     public override async Task<OnPlayerSpawnArguments> OnPlayerSpawning(MyPlayer player, OnPlayerSpawnArguments request)
     {
-        if (player.IsZombie)
+        if (_vip == null)
+            // no VIP do not do anything yet. (The challenge kills everyone anyway when starting :) )
+            return request;
+
+        if (player.Team != _vip.Team)
         {
+            // Only give them a hammer
             request.Loadout.PrimaryWeapon = default;
             request.Loadout.SecondaryWeapon = default;
             request.Loadout.LightGadget = null;
@@ -87,25 +113,34 @@ class MyGameServer : GameServer<MyPlayer>
 
         return request;
     }
+
     public override async Task OnPlayerSpawned(MyPlayer player)
     {
-        if(player.IsZombie)
+        if (player == _vip)
         {
             player.SetRunningSpeedMultiplier(2f);
-            player.SetJumpMultiplier(2f);
-            player.SetFallDamageMultiplier(0f);
-            player.SetReceiveDamageMultiplier(0.1f);
+            player.SetReceiveDamageMultiplier(0.5f);
             player.SetGiveDamageMultiplier(4f);
         }
+        else if (_vip.Team == player.Team)
+        {
+            player.SetRunningSpeedMultiplier(0.6f);
+            player.SetReceiveDamageMultiplier(2f);
+            player.SetGiveDamageMultiplier(0.25f);
+            player.Message($"The VIP {player.Name} is in your Team. Protect them! You may use all weapons.");
+        }
+        else
+        {
+            player.Message($"{player.Name} in the enemy team is the VIP. Kill them!");
+        }
     }
-
-
 
     public override async Task OnConnected()
     {
         await Console.Out.WriteLineAsync("Current state: " + RoundSettings.State);
-
+        RoundSettings.PlayersToStart = NeededPlayers;
     }
+
     public override async Task OnGameStateChanged(GameState oldState, GameState newState)
     {
         await Console.Out.WriteLineAsync("State changed to -> " + newState);
